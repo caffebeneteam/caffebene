7ee0638755899f7293da4e68477465e6e38e7d0d
heda-art <72438648+heda-art@users.noreply.github.com>
2020-10-27 12:11:33 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,50 +29,132 @@ $(document).ready(function(){
 
 });
 
-// Pick slider
-$(function () {
-  var left = 0;
-  var duration = 3000;
-  var slider;
-  
-  function changeSlider(paramLeft) {
-    $('.pager > a').removeClass('active');
-    $('.pager > a').eq(paramLeft).addClass('active');
+$(function() {
+
+  //이 부분을 자신의 상황에 맞게 수정
+  var width = 1300; //슬라이드 한 개의 폭
+  var height = 650; //슬라이드 높이
+  var animationSpeed = 1000; //화면전환 속도
+  var pause = 3000; //화면전환 후 일시 정지 속도
+  var totalSlides = 7; //복제 슬라이드를 포함한 전체 슬라이드 개수
+
+  var currentSlide = 2; //이 항목은 수정하지 않음
+  var interval;
+  var action;
+  var dotNum;
+  var dMinusC;
+
+  //cache DOM elements
+  var $slideCon = $('#slider');
+  var $slideUl = $('.slides');
+  var $slides = $('.slide');
+  var $dots = $('.slider-nav-dot');
+  var $sliderNavPrv = $('#slider-nav-prv');
+  var $sliderNavNxt = $('#slider-nav-nxt');
+
+  function initSlider(){
+    $slideCon.css('width',width);
+    $slideCon.css('height',height);
+    $slideUl.css('margin-left',-width);
+    $slideUl.css('width',totalSlides*width);
+    $slides.css('width',width);
+    $slides.css('height',height);
   }
 
-  function sliderStart() {
-    slider = setInterval(function () {
-      $("#pick > .grid").hide();
-      if(left > 1) {
-        left = 0;
-      }
-      else {
-        left = left + 1;
-      }
-      //$('#pick > ul > li').css('left', left);
-      $("#pick > .grid").eq(left).show();
-      console.log(left);
-      changeSlider(left);
-    }, duration);
-  }
+  function startSlider(action, dotNum) {
 
-  // 슬라이더 정지
-  function sliderStop() {
-    clearInterval(slider);
-    // console.log('stop');
-  }
+    if(action == 'prv'){
+      $slideUl.animate({'margin-left': '+='+width}, animationSpeed, function() {
+        if (--currentSlide === 1) {
+          currentSlide = $slides.length-1;
+          $slideUl.css('margin-left', -($slides.length-2)*width);
+        }else{}
 
-  sliderStart();
+        for(var i=0; i<$dots.length; i++){$dots[i].style.background = "";}
+        $dots[currentSlide-2].style.background = "#203053";
+      });
+    } else if(action == 'nxt') {
+      $slideUl.animate({'margin-left': '-='+width}, animationSpeed, function() {
+        if (++currentSlide === $slides.length) {
+          currentSlide = 2;
+          $slideUl.css('margin-left', -width);
+        }else{}
 
-  // 마우스가 올라가면 슬라이더 정지
-  $('.grid, .pager').hover(
-    function() {
-      sliderStop();
-    },
-    function() {
-      sliderStart();
-      // console.log('start');
+        for(var i=0; i<$dots.length; i++){$dots[i].style.background = "";}
+        $dots[currentSlide-2].style.background = "#203053";
+      });
+    } else if(action == 'dot') {
+      dMinusC = dotNum-currentSlide;
+      currentSlide = dotNum;
+
+      for(var i=0; i<$dots.length; i++){$dots[i].style.background = "";}
+      $dots[currentSlide-2].style.background = "#203053";
+
+      $slideUl.animate({'margin-left': '-='+(dMinusC*width)}, animationSpeed);
+
+    } else {
+      // setInterval(function,milliseconds)
+      // 지정한 시간에 한번씩 함수를 실행
+      // 3초 마다 $slideUl 의 왼쪽 마진을 -width 함. 에니메이션 속도는 1초.
+      interval = setInterval(function() {
+
+        //.animate( CSS properties [, duration ] [, complete ] )
+        $slideUl.animate({'margin-left': '-='+width}, animationSpeed, function() {
+          if (++currentSlide === $slides.length) { // $slides.length == 7
+            currentSlide = 2;
+            $slideUl.css('margin-left', -width);
+          }
+
+          for(var i=0; i<$dots.length; i++){$dots[i].style.background = "";}
+          $dots[currentSlide-2].style.background = "#203053";
+        });	
+
+      }, pause);
     }
-  )
 
-}) //$end
+  }
+
+
+  function pauseSlider() {
+    clearInterval(interval);
+  }
+
+  function prvSlide(){
+    startSlider('prv');
+  }
+
+  function nxtSlide(){
+    startSlider('nxt');
+  }
+
+  function dotSelected(){
+    dotNum = $(this).attr('id');
+    dotNum = parseInt(dotNum.substring(7))+1;
+    startSlider('dot', dotNum);
+  }
+
+
+  $slideUl
+    .on('mouseenter', pauseSlider)
+    .on('mouseleave', startSlider);
+
+  $sliderNavPrv
+    .on('click',prvSlide)
+    .on('mouseenter', pauseSlider)
+    .on('mouseleave', startSlider);
+
+  $sliderNavNxt
+    .on('click',nxtSlide)
+    .on('mouseenter', pauseSlider)
+    .on('mouseleave', startSlider);
+
+  $dots
+    .on('click', dotSelected)
+    .on('mouseenter', pauseSlider)
+    .on('mouseleave', startSlider);		
+
+  
+  initSlider();
+  startSlider();
+
+});
